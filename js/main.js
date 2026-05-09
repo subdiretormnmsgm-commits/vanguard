124e3c52ee5d683d3d296740698e44d5b0cfcd18
Eduardo <subdiretor.mnmsgm@gmail.com>
2026-05-09 02:20:13 -0300

    feat(bootstrap): add SW registration and quiz init

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  }
+  Quiz.init();
+});
